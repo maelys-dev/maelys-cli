4bf45d5156910bf0ab9669c1bd218448329a9c1f
David Bromberg <davidb987654@gmail.com>
2026-09-05 12:40:45 +0200

    release: prepare 0.5.13

--- include/maelys/cli/version.h
+++ include/maelys/cli/version.h
@@ -1,10 +1,10 @@
 #ifndef MAELYS_CLI_VERSION_H
 #define MAELYS_CLI_VERSION_H
 
-#define MAELYS_CLI_VERSION "0.5.12"
+#define MAELYS_CLI_VERSION "0.5.13"
 #define MAELYS_CLI_VERSION_MAJOR 0
 #define MAELYS_CLI_VERSION_MINOR 5
-#define MAELYS_CLI_VERSION_PATCH 12
+#define MAELYS_CLI_VERSION_PATCH 13
 
 /* Link-level ABI generation of libmaelys_cli. */
 #define MAELYS_CLI_ABI 1
